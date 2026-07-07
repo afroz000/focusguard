69cd3947c4180962a810e987a179728e998ea4bd
Afroz <pafrozkhan@gmail.com>
2026-07-07 20:43:03 +0530

    FG-101 Initialize FocusGuard Spring Boot project

---
+++ src/main/java/com/afroz/focusguard/FocusguardApplication.java
@@ -0,0 +1,13 @@
+package com.afroz.focusguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusguardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/afroz/focusguard/FocusguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afroz.focusguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
